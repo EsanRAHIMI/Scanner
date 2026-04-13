0ad356422911b24d7404895d30e079849d2fb71a
Esan <ehsanrahimi@live.com>
2026-04-13 11:17:52 +0400

    Update UI/UX SocialFeed7

--- products/app/products/page.tsx
+++ products/app/products/page.tsx
@@ -60,55 +60,57 @@ export default function ProductsPage() {
         />
       </div>
 
-      <ProductsView
-        titleNode={
-          <div className="flex h-10 min-w-0 max-w-[340px] items-center gap-3 overflow-hidden">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              src={`${basePath}/Logo1.png`}
-              alt="Lorenzo's products"
-              className={
-                (logo1Loaded ? 'opacity-100 ' : 'opacity-0 ') +
-                'h-9 w-auto max-w-full object-contain transition-opacity dark:hidden'
-              }
-            />
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              src={`${basePath}/Logo2.png`}
-              alt="Lorenzo's products"
-              className={
-                (logo2Loaded ? 'opacity-100 ' : 'opacity-0 ') +
-                'hidden h-9 w-auto max-w-full object-contain transition-opacity dark:block'
-              }
-            />
-            {!(logo1Loaded || logo2Loaded) ? <span className="text-2xl font-semibold">Lorenzo's products</span> : null}
-          </div>
-        }
-        mobileTitleNode={
-          <span className="flex h-10 min-w-0 max-w-[140px] flex-none items-center overflow-hidden">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              src={`${basePath}/Logo Sign Black.png`}
-              alt="Lorenzo's products"
-              className={
-                (logoSignBlackLoaded ? 'opacity-100 ' : 'opacity-0 ') +
-                'h-7 w-auto max-w-full object-contain transition-opacity dark:hidden'
-              }
-            />
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              src={`${basePath}/Logo Sign White.png`}
-              alt="Lorenzo's products"
-              className={
-                (logoSignWhiteLoaded ? 'opacity-100 ' : 'opacity-0 ') +
-                'hidden h-7 w-auto max-w-full object-contain transition-opacity dark:block'
-              }
-            />
-            {!(logoSignBlackLoaded || logoSignWhiteLoaded) ? <span className="truncate text-lg font-semibold">Lorenzo's</span> : null}
-          </span>
-        }
-        title="Lorenzo's products"
-      />
+      <React.Suspense fallback={null}>
+        <ProductsView
+          titleNode={
+            <div className="flex h-10 min-w-0 max-w-[340px] items-center gap-3 overflow-hidden">
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                src={`${basePath}/Logo1.png`}
+                alt="Lorenzo's products"
+                className={
+                  (logo1Loaded ? 'opacity-100 ' : 'opacity-0 ') +
+                  'h-9 w-auto max-w-full object-contain transition-opacity dark:hidden'
+                }
+              />
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                src={`${basePath}/Logo2.png`}
+                alt="Lorenzo's products"
+                className={
+                  (logo2Loaded ? 'opacity-100 ' : 'opacity-0 ') +
+                  'hidden h-9 w-auto max-w-full object-contain transition-opacity dark:block'
+                }
+              />
+              {!(logo1Loaded || logo2Loaded) ? <span className="text-2xl font-semibold">Lorenzo's products</span> : null}
+            </div>
+          }
+          mobileTitleNode={
+            <span className="flex h-10 min-w-0 max-w-[140px] flex-none items-center overflow-hidden">
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                src={`${basePath}/Logo Sign Black.png`}
+                alt="Lorenzo's products"
+                className={
+                  (logoSignBlackLoaded ? 'opacity-100 ' : 'opacity-0 ') +
+                  'h-7 w-auto max-w-full object-contain transition-opacity dark:hidden'
+                }
+              />
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                src={`${basePath}/Logo Sign White.png`}
+                alt="Lorenzo's products"
+                className={
+                  (logoSignWhiteLoaded ? 'opacity-100 ' : 'opacity-0 ') +
+                  'hidden h-7 w-auto max-w-full object-contain transition-opacity dark:block'
+                }
+              />
+              {!(logoSignBlackLoaded || logoSignWhiteLoaded) ? <span className="truncate text-lg font-semibold">Lorenzo's</span> : null}
+            </span>
+          }
+          title="Lorenzo's products"
+        />
+      </React.Suspense>
     </div>
   );
 }
